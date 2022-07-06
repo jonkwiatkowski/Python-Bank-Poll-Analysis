a107e3390a3392bd6d3841d3bf11949dbb2f93dd
Jon Kwiatkowski <jon.kwiatkowski@me.com>
2022-07-06 12:53:59 -0500

    PyBank Complete and README edited.

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,12 +3,17 @@ import csv
 
 csvpath = os.path.join("Resources","budget_data.csv")
 
+# Create empty lists
 Date = []
 ProfitLoss = []
 Change = []
 
+# Open and read csv
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
+
+    # Store the header
+    csv_header = next(csvfile)
 
     for row in csvreader:
 
@@ -16,20 +21,18 @@ with open(csvpath) as csvfile:
 
         ProfitLoss.append(row[1])
 
-    # Make sure that the headers aren't included in Date or ProfitLoss lists
-    del Date[0]
-    del ProfitLoss[0]
-
     # Cast list as integers
     ProfitLoss = [int(i) for i in ProfitLoss]
 
     # Build list containing changes in profit/loss
     x = 0
+
     while x < (len(ProfitLoss)-1):
 
         Change.append(ProfitLoss[x+1]-ProfitLoss[x])
         x = x + 1
 
+    # Define necessary variables
     NumberDates = len(Date)
 
     Total = sum(int(x) for x in ProfitLoss)
@@ -43,10 +46,10 @@ with open(csvpath) as csvfile:
     GreatestDecrease = min(Change)
 
     GreatestIncreaseDate = Date[Change.index(GreatestIncrease)+1]
+
     GreatestDecreaseDate = Date[Change.index(GreatestDecrease)+1]
 
     # Print results
-
     print("Financial Analysis")
 
     print("------------------------------------------------")
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # python-challenge
 My submission for Homework Assignment 3.
+## PyBank
+Analysis of the financial records for a bank. The dataset involved contains "Date" and Profit/Loss"
+
+A python script was created to calculate the following values:
+
+Total number of months included in the dataset
+
+Net total amount of "Profit/Losses" over the entire period
+
+Changes in "Profit/Losses" over the entire period and the average of those changes
+
+Greatest increase in profits (date and amount) over the entire period
+
+Greatest decrease in profits (date and amount) over the entire period
+
+## PyPoll
+
+Analyze a set of poll data for a small, rural town. The data set contains the following information: "Voter ID", "County", and "Candidate".
+
+A Python script was created to calculate the following values:
+
+Total number of votes cast
+
+List of candidates who received votes
+
+Percentage of votes each candidate won
+
+Total number of votes each candidate won
+
+Winner of the election based on popular vote
